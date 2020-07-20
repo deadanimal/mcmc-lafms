fa1e8121315c3f778de3f8a8f7129e30979ca933
Nameyusliadiyusof <yusliadiyusof@pipeline.com.my>
2020-07-21 00:05:01 +0800

    ynbgtft gvy

--- mcmc-lafms-web/src/environments/environment.prod.ts
+++ mcmc-lafms-web/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   // baseUrl: 'https://www.api.prototype.com.my/',
-  baseUrl: "https://mcmc-lafms-api.pipe.com.my/",
+  baseUrl: "https://mcmc-lafms-api.pipe.my/",
   mapbox: {
     accessToken:
       "pk.eyJ1IjoiYWZlZXpheml6IiwiYSI6ImNqNjJ6anlhYzA0bTczM3FvYnppbDh4eTEifQ.AdDRr42bNfNJvQENLrE6eg", // Your access token goes here
